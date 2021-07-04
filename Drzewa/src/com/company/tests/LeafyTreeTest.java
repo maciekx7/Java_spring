42965885798b2b1a88c59ee231ed18d7f7a8f584
maciekx7 <52274468+maciekx7@users.noreply.github.com>
2021-07-04 14:08:18 +0200

    Update LeafyTreeTest.java

--- Drzewa/src/com/company/tests/LeafyTreeTest.java
+++ Drzewa/src/com/company/tests/LeafyTreeTest.java
@@ -28,15 +28,15 @@ class LeafyTreeTest extends TreeTest {
     void growTesting() {
         LeafyTree leafyTree = (LeafyTree) tree;
         assertEquals(0, tree.GetBranchesNumber(), "Branches should be 0 before first growing");
-        assertEquals(0,leafyTree.GetLeavesNumber(),"Needles should be 0 before first growing");
+        assertEquals(0,leafyTree.GetLeavesNumber(),"Leaves should be 0 before first growing");
 
         leafyTree.grow(2);
         assertEquals(2, tree.GetBranchesNumber(), "Branches should be 2 after first growing");
-        assertEquals((2*2),leafyTree.GetLeavesNumber(),"Needles should be 4 after first growing");
+        assertEquals((2*2),leafyTree.GetLeavesNumber(),"Leaves should be 4 after first growing");
 
         leafyTree.grow(3);
         assertEquals((2+3), tree.GetBranchesNumber(), "Branches should be 2 after second growing");
-        assertEquals((2*2+3*2),leafyTree.GetLeavesNumber(),"Needles should be 4 after second growing");
+        assertEquals((2*2+3*2),leafyTree.GetLeavesNumber(),"Leaves should be 4 after second growing");
     }
 
 }
